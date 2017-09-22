a4ada42c69063aea1b335e0caf659b1793203900
vasco <vastur@gmail.com>
2017-09-21 21:21:34 -0700

    Upgrade kapsule

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,24 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default {
+    entry: 'src/index.js',
+    dest: 'dist/d3-force-pod.js',
+    format: 'umd',
+    moduleName: 'd3ForcePod',
     plugins: [
         nodeResolve({
             jsnext: true,
             main: true
         }),
-        babel()
+        commonJs(),
+        babel({
+            presets: [
+                ["es2015", { "modules": false }]
+            ],
+            plugins: ["external-helpers"],
+            babelrc: false
+        })
     ]
 };
